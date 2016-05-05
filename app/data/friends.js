16763558df2e530d2f5f62011cbfd52a51ba246a
winniemlau <wmlau26@gmail.com>
2016-05-04 21:12:02 -0400

    additional commits

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendsArray = [
+	{
+    "name":"Pooh",
+    "photo":"http://vignette1.wikia.nocookie.net/fictionalcrossover/images/0/03/A_winnie_the_pooh.png",
+    "scores":[
+       5,
+       1,
+       4,
+       4,
+       5,
+       1,
+       2,
+       5,
+       4,
+       1
+    ]
+
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
---
+++ app/routing/html-routes.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = function(app){
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	app.get('/home', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+}
